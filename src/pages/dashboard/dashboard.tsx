df85d6d890a050d56567ed80a10d45ee63a7a322
patrick <patrick.estagiario@ms.touk.io>
2023-06-17 12:26:07 -0300

    add dashboard page

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/navbar';
+import Header from '../../components/Header/header';
+import SidebarMenu from '../../components/SideMenu/sidemenu';
+import SearchBar from '../../components/SearchBar/searchbar';
+import CustomButton from '../../components/CustomButton/custombutton';
+import buttonIcon from '../../assets/avatar.png'
+import './dashboard.css'
 
-const username = "Fábio Bala"
+
+const username = "Fábio Bala";
 
 const DashboardPage = () => {
   return (
     <div>
       <Navbar username={username}/>
-      {/* Conteúdo da página de Dashboard */}
+      <Header />
+      <SidebarMenu />
+      <div className="searchbar">
+        <SearchBar />
+      </div>
+      <div className="button-div">
+        <div className='button-div-row1'>
+        <CustomButton text="Nutricionistas"/>
+        <CustomButton text="Academias" />
+        </div>
+        <div className='button-div-row2'>
+        <CustomButton text="Playlists" />
+        <CustomButton text="Personal" />
+        </div>
+      </div>
     </div>
   );
 }
