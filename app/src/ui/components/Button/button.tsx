b7fd8f2a6337863c398cb3de6c0934a7be7554cb
Fabíola Diniz <fabioladiniz36@hotmail.com>
2023-06-26 14:58:35 -0300

    Update button.tsx
    atualização  de button

--- app/src/ui/components/Button/button.tsx
+++ app/src/ui/components/Button/button.tsx
@@ -1,32 +1,103 @@
-import React from 'react';
-
-interface ButtonProps {
-  buttonColor: string;
-  textColor: string;
-  buttonText: string;
-  width?: string;
-  height?: string;
-  fontSize?: string;
-  type: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
-  onClick?: () => void;
+.page {
+  background-color: #90E0EF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+  text-align: center; /* Adicionado para centralizar o texto */
 }
 
-const Button: React.FC<ButtonProps> = ({ buttonColor, textColor, buttonText, width, height, fontSize, type, onClick }) => {
-  const buttonStyle = {
-    backgroundColor: buttonColor,
-    color: textColor,
-    border: 'none',
-    width: width || 'auto',
-    height: height || 'auto',
-    fontSize: fontSize || 'inherit',
-    borderRadius: '3px'
-  };
-
-  return (
-    <button style={buttonStyle} type={type} onClick={onClick}>
-      {buttonText}
-    </button>
-  );
+.container {
+  width: 400px;
+  height: 480px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.4);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
 }
 
-export default Button;
+.title {
+  color: #03045E;
+  font-size: 20px;
+  font-weight: normal;
+  margin-bottom: 5px;
+}
+
+.bold {
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.logo {
+  width: 100px;
+  height: 100px;
+  margin-bottom: 10px;
+}
+
+.welcome-text {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.space {
+  margin: 2px; 
+}
+
+.gray-text {
+  color: gray;
+  font-size: 16px;
+  margin-bottom: 5px;
+  font-weight: 700;
+  font-family: 'Poetsen One', sans-serif;
+
+                                                
+}
+
+.blue-text {
+  color: #03045E;
+  font-size: 16px;
+  font-weight: 850;
+  margin-bottom: 17px;
+  font-family: 'Poetsen One', sans-serif;
+                                                
+}
+
+.input-group {
+  margin-bottom: 20px;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+.form-group .gray-text {
+  color: gray;
+  font-size: 14px;
+  margin-right: 80%;
+}
+
+.form-group .blue-text {
+  color: #03045E;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.button-group {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+}
+
+.button-group button {
+  margin-bottom: 10px;
+}
